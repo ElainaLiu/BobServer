8a0eb102d830c10997f1b583dbb08996ed893cf9
Elaina Liu <elainaliu15@gmail.com>
2020-01-22 17:22:20 -0500

    Update User.java
    Removed redundant properties

--- src/main/java/com/bobserver/bobserver/model/User.java
+++ src/main/java/com/bobserver/bobserver/model/User.java
@@ -7,8 +7,6 @@ public class User {
 
     private String username;
     private HashMap<String, Double> attributes;
-    private String[] attributeNames;
-    private double[] attributeVals;
     private String playlistUrl;
 
     public User(String name, String[] attributeNames, double[] attributeVals, String url){
@@ -17,8 +15,6 @@ public class User {
         for(int i = 0; i < attributeNames.length; i++){
             this.attributes.put(attributeNames[i], attributeVals[i]);
         }
-        this.attributeNames = attributeNames;
-        this.attributeVals = attributeVals;
         this.playlistUrl = url;
     }
 
@@ -28,14 +24,6 @@ public class User {
 
     public Map<String, Double> getAttributeMap(){
         return this.attributes;
-    }
-
-    public String[] getAttributes(){
-        return this.attributeNames;
-    }
-
-    public double[] getVals(){
-        return this.attributeVals;
     }
 
     public String getUrl(){
