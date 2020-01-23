bce4cd21b3bfc9eeed14dc97585496a489f73b22
Elaina Liu <elainaliu15@gmail.com>
2020-01-23 11:10:13 -0500

    Final demo updates
    Added annotation to hook up to frontend server, changed @username to username, changed playlists and personality values

--- src/main/java/com/bobserver/bobserver/Controller.java
+++ src/main/java/com/bobserver/bobserver/Controller.java
@@ -5,7 +5,12 @@ import com.bobserver.bobserver.model.UserGroup;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+import java.util.*;
+
+
+@CrossOrigin(origins = { "http://localhost:3000"})
 @RestController
 public class Controller {
     @GetMapping("/api/intro")
@@ -14,13 +19,13 @@ public class Controller {
     }
 
     @GetMapping("/username")
-    public User usernameCall(@RequestParam(value = "username", defaultValue = "@JeffBezos") String username){
+    public Map<String, Double> usernameCall(@RequestParam(value = "username", defaultValue = "JeffBezos") String username){
         UserGroup group = new UserGroup(username);
-        return group.getCurrUser();
+        return group.getCurrUser().getAttributeMap();
     }
 
     @GetMapping("/playlist")
-    public String playlistCall(@RequestParam(value = "username", defaultValue = "@JeffBezos") String username){
+    public String playlistCall(@RequestParam(value = "username", defaultValue = "JeffBezos") String username){
         UserGroup group = new UserGroup(username);
         return group.getCurrUser().getUrl();
     }
--- src/main/java/com/bobserver/bobserver/model/UserGroup.java
+++ src/main/java/com/bobserver/bobserver/model/UserGroup.java
@@ -11,17 +11,17 @@ public class UserGroup {
     public UserGroup(String name){
         String[] atts = {"Openness", "Conscientiousness", "Extraversion", "Agreeableness", "Emotional Range"};
         
-        double[] vals1 = {40.7, 80.3, 2.2, 67.8, 34.9};
-        double[] vals2 = {70.9, 60.2, 40.3, 3.2, 12.4};
-        double[] vals3 = {10.5, 50.3, 20.3, 27.5, 0.5};
+        double[] vals1 = {85.7, 80.3, 42.2, 67.8, 24.9};
+        double[] vals2 = {70.9, 20.2, 40.3, 3.2, 82.4};
+        double[] vals3 = {10.5, 50.3, 80.3, 47.5, 0.5};
         
-        String user1 = "@JeffBezos";
-        String user2 = "@Tim_Cook";
-        String user3 = "@OSUPrezDrake";
+        String user1 = "JeffBezos";
+        String user2 = "Tim_Cook";
+        String user3 = "OSUPrezDrake";
         
-        String url1 = "https://open.spotify.com/playlist/5aGXIuWyYg15pUlcRgvRmo?si=FgyUo_FeTaOBxiw0zg2kfQ"; //Classical Jazz
-        String url2 = "https://open.spotify.com/playlist/27gN69ebwiJRtXEboL12Ih?si=9OYHOUmiS_mp2YPDE8lTmA"; //Heavy metal
-        String url3 = "https://open.spotify.com/playlist/08DYV5GhZ92P5YaJkLG2bw?si=bAyGA8llShuSszv5VPew1Q"; //Pop/upbeat
+        String url1 = "https://open.spotify.com/playlist/5EF18jWof9VRYaC7ss3Wte"; //Classical Jazz
+        String url2 = "https://open.spotify.com/playlist/0RKi2UxVDVMrZqBA9uRSuT"; //Heavy metal
+        String url3 = "https://open.spotify.com/playlist/5KMBhQon3ArWQe2vPzC8KW"; //Pop/upbeat
         
         this.users.put(user1, new User(user1, atts, vals1, url1));
         this.users.put(user2, new User(user2, atts, vals2, url2));
